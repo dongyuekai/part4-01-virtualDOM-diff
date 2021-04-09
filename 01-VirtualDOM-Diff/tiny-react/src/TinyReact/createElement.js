82d9ae86989e1c94c3bcbc5a8703a7f6ef38bdb1
“dongyuekai” <457545648@qq.com>
2021-04-09 16:37:20 +0800

    自定义实现createElement

---
+++ 01-VirtualDOM-Diff/tiny-react/src/TinyReact/createElement.js
@@ -0,0 +1,34 @@
+export default function createElement(type, props, ...children) {
+
+  // const childElements = [].concat(...children).map(child => {
+  //   if (child instanceof Object) {
+  //     return child
+  //   } else {
+  //     return createElement('text', { textContent: child })
+  //   }
+  // })
+
+  // return {
+  //   type,
+  //   props,
+  //   children: childElements
+  // }
+
+  // bool值为false的节点不显示
+  const childElements2 = [].concat(...children).reduce((result, child) => {
+    if (child !== false && child !== true && child !== null) {
+      if (child instanceof Object) {
+        result.push(child)
+      } else {
+        result.push(createElement('text', { textContent: child }))
+      }
+    }
+    return result
+  }, [])
+  return {
+    type,
+    // 在props中添加children属性
+    props: Object.assign({ children: childElements2}, props),
+    children: childElements2
+  }
+}
